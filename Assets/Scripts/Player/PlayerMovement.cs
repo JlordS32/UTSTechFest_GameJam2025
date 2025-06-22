b984b5a0e9b87525d2b44b0b713bc49c048cb782
JlordS32 <jlordsty128@gmail.com>
2025-06-22 23:43:44 +1000

    setup

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,8 +3,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public void OnMove()
+    // PROPERTIES
+    [SerializeField] float _moveSpeed = 5f;
+
+    // VARIABLES
+    Rigidbody2D _rb;
+    Vector2 _moveInput;
+
+    void Awake()
     {
-        Debug.Log("Button Pressed");
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _rb.linearVelocity = _moveInput * _moveSpeed;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
     }
 }
