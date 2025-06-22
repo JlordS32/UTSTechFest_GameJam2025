a5ab9dd9d4841c7043a3fec938250aee337f4520
JlordS32 <jlordsty128@gmail.com>
2025-06-22 23:23:21 +1000

    initial setup

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public void OnMove()
+    {
+        Debug.Log("Button Pressed");
+    }
+}
