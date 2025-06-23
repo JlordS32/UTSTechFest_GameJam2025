5fab2394a91b48553f4d1b738691ae0463675cf3
DuyNguyen16 <khanhduyn161@gmail.com>
2025-06-23 22:34:47 +1000

    map

---
+++ Assets/Scripts/Map/PathTile.cs
@@ -0,0 +1,14 @@
+
+public class PathTile
+{
+    public int x;
+    public int y;
+    public bool walkable;
+
+    public PathTile(int x, int y, bool walkable)
+    {
+        this.x = x;
+        this.y = y;
+        this.walkable = walkable;
+    }
+}
